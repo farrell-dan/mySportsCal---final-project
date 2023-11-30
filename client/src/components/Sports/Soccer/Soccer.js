05cef7ea4a819499213af07653d83726064b58ad
Daniel Farrell <farrelld94@live.com>
2023-11-30 18:47:20 -0500

    Soccer CSS

--- client/src/components/Sports/Soccer/Soccer.js
+++ client/src/components/Sports/Soccer/Soccer.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./Soccer.css";
 
 const SoccerHome = () => {
 	const logoStyle = {
@@ -20,53 +21,13 @@ const SoccerHome = () => {
 	return (
 		<>
 			<h1>Soccer</h1>
-
+<div className="leagues-container">
 			{leagues.map((league) => (
-				<Link key={league.path} to={league.path}>
+				<Link key={league.path} to={league.path} className="league-link" >
 					<img src={`/assets/LeagueLogos/Soccer/${league.name.replace(/\s/g, '')}Logo.png`} alt={`${league.name} Logo`} style={logoStyle} />
 				</Link>
 			))}
-			{/* <Link to="/epl">
-				<img src="/assets/LeagueLogos/Soccer/PremierLeagueLogo.png" alt="EPL Logo" style={logoStyle} />
-				English Premier League
-			</Link>
-			<Link to="/laliga">
-				<img src="/assets/LeagueLogos/Soccer/LaLigaLogo.svg.png" alt="La Liga Logo" style={logoStyle} />
-				La Liga
-			</Link>
-			<Link to="/bundesliga">
-				<img
-					src="/assets/LeagueLogos/Soccer/BundesligaLogo.svg.png"
-					alt="Bundesliga Logo"
-					style={logoStyle}
-				/>
-				Bundesliga
-			</Link>
-			<Link to="/seriea">
-				<img src="/assets/LeagueLogos/Soccer/SerieALogo.png" alt="Serie A Logo" style={logoStyle} />
-				Serie A
-			</Link>
-			<Link to="/ligue1">
-				<img src="/assets/LeagueLogos/Soccer/Ligue1Logo.svg.png" alt="Ligue 1 Logo" style={logoStyle} />
-				Ligue 1
-			</Link>
-			<Link to="/championsleague">
-				<img
-					src="/assets/LeagueLogos/Soccer/UEFAChampionsLeagueLogo.svg.png"
-					alt="Champions League Logo"
-					style={logoStyle}
-				/>
-				Champions League
-			</Link>
-			<Link to="/europaleague">
-				<img
-					src="/assets/LeagueLogos/Soccer/UEFAEuropaLeagueLogo.svg.png"
-					alt="Europa League Logo"
-					style={logoStyle}
-				/>
-				Europa League
-			</Link> */}
-			<h2>MLS</h2>
+			</div>
 		</>
 	);
 };
