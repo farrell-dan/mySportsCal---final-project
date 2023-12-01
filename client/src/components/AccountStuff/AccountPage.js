8a695e1e4fa1596b44e3e4fb9bf666026adfb7de
Daniel Farrell <farrelld94@live.com>
2023-11-30 19:42:20 -0500

    Fix Account Page

--- client/src/components/AccountStuff/AccountPage.js
+++ client/src/components/AccountStuff/AccountPage.js
@@ -2,22 +2,35 @@ import Login from "./Login";
 import React, { useState } from "react";
 import "./Forms.css";
 import CreateAccount from "./CreateAccount";
+import { useAuth } from "./AuthProvider";
+import MyEvents from "./MyEvents";
 
 const AccountPage = () => {
+	const { authenticated } = useAuth();
 	const [currentForm, setCurrentForm] = useState("login");
 
 	const toggleForm = (formName) => {
 		setCurrentForm(formName);
 	};
 
-	return (
-		<div>
-			<h1>AccountPage</h1>
-			{currentForm === "login" ? (
+	const renderAuthForm = () => {
+		if(!authenticated){
+			return currentForm === "login" ? (
 				<Login onFormSwitch={toggleForm} />
 			) : (
 				<CreateAccount onFormSwitch={toggleForm} />
-			)}
+			)
+		}
+		return (
+			<div className="container">
+			<h2>My Upcoming Games</h2> <MyEvents />
+			</div>
+		)
+	}
+	return (
+		<div>
+			<h1>AccountPage</h1>
+			{renderAuthForm()}
 		</div>
 	);
 };
